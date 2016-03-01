d4f8a8bcd8617d945b0cdfa9270e46bceff198bd
Seunghwan Yoo <seunghwanyoo2013@u.northwestern.edu>
2016-03-01 16:19:28 -0600

    exercise for function template

---
+++ fun_template.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// implement fun_sum function here
+
+
+// implement fun_max function here
+
+
+template<typename T>
+void print(T a, const string& s) {
+	cout << s << a << endl;
+}
+
+int main() {
+	int ai = 1, bi = 2;
+	double ad = 1.2, bd = 3.4;
+
+	int sumi = 0, maxi = 0;
+//	sumi = fun_sum(ai,bi);
+//	maxi = fun_max(ai,bi);
+	print(sumi,"int: ai + bi = ");
+	print(maxi,"int: max(ai,bi) = ");
+
+	double sumd = 0, maxd = 0;
+//	sumd = fun_sum(ad,bd);
+//	maxd = fun_max(ad,bd);
+	print(sumd, "double: ad + bd = ");
+	print(maxd, "double: max(ad,bd) = ");
+
+	return 0;
+}
+	
